fcdbb746037231276e64fe3b23dfd5a66ed9eca3
Brandon Trebitowski <brandontreb@gmail.com>
2015-01-29 10:49:10 -0700

    Initial Commit

---
+++ TableViewDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TableViewDemo
+//
+//  Created by Brandon Trebitowski on 1/29/15.
+//  Copyright (c) 2015 Pixegon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TableViewDemo/MasterViewController.h
@@ -0,0 +1,15 @@
+//
+//  MasterViewController.h
+//  TableViewDemo
+//
+//  Created by Brandon Trebitowski on 1/29/15.
+//  Copyright (c) 2015 Pixegon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MasterViewController : UITableViewController
+
+
+@end
+
